7580a356f9224925ae8732ba4fd803e91005a196
Akogwu <akogwu@Akogwus-MacBook-Pro.local>
2023-02-11 00:15:25 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  const x = 6;
+
+  return (
+    <div className='App'>
+      <h1>Workingjjj</h1>
+    </div>
+  );
+}
+
+export default App;
